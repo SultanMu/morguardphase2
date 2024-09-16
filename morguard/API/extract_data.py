834fd5f775fc8ec00d734d3942b9ddb296061e35
hassaan <hassaan@ivedha>
2024-09-16 20:38:44 +0500

    docx-fileV2

--- morguard/API/extract_data.py
+++ morguard/API/extract_data.py
@@ -37,7 +37,7 @@ class ExtractData:
     
     async def index_file(self,url:str,file_name:str,folder_name:str):
         async with aiohttp.ClientSession() as session:            
-            async with session.post("http://mguard-file-processor:8000/process/",data={"content":url}) as response:
+            async with session.post("http://mguard-file-processor-phase-2:8000/process/",data={"content":url}) as response:
                 response.raise_for_status()
                 texts= await response.json()
                 text_data = texts.get("texts",[])
